e212db8e1ae1d19b7c173554597b3d54956dd8a3
Ezenwa Ogbonna <ezenwaogbonna@Ezenwas-MacBook-Pro.local>
2021-05-02 18:49:44 +0100

    upload developing your discoveries

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,8 @@ class Book < ApplicationRecord
   mount_uploader :book_url, FileUploader
   mount_uploader :image_url, FileUploader
   validates_presence_of :name, :summary
+
+  def download_url
+    self[:book_url].include?("github") ? self[:book_url] : book_url
+  end
 end
